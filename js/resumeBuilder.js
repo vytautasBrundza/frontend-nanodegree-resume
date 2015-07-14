976adb3540d0942ae09fafe76a6f4b375f707bdf
Vytautas Brundza <vytautas.brundza@cohaesus.co.uk>
2015-07-14 12:13:15 +0100

    imlemented work and projects sections

--- js/resumeBuilder.js
+++ js/resumeBuilder.js
@@ -1,4 +1,9 @@
-// REPLACE DATA FOR BIO
+// Helper function (http://stackoverflow.com/questions/4364881/inserting-string-at-position-x-of-another-string)
+String.prototype.insertAt=function(index, string) {
+  return this.substr(0, index) + string + this.substr(index);
+}
+
+// BIO
 function FillBio(JSONbio)
 {
 	console.log("replacing bio data");
@@ -50,7 +55,6 @@ function FillBio(JSONbio)
 	}
 }
 
-// LOAD BIO
 $.ajax({
 url: "data/bio.json",
 success: function (data) {
@@ -60,7 +64,7 @@ success: function (data) {
 },
 error: function (data) {
   console.log("could not load bio JSON file");
-  HideHeader();
+  HideBio();
 }
 });
 
@@ -68,8 +72,106 @@ function HideBio()
 {
 	if(document.getElementsByTagName('h1').length === 0) {
       document.getElementById('header').style.display = 'none';
+      console.log("header hidden");
     }
     if(document.getElementsByClassName('flex-item').length === 0) {
       document.getElementById('topContacts').style.display = 'none';
+      console.log("top contacts hidden");
     }
 }
+
+// WORK
+function FillWork(JSONwork)
+{
+	console.log("replacing work data");
+	if(JSONwork.jobs){
+		for (i = 0; i < JSONwork.jobs.length; i++) {
+			// create single work HTML
+			var HTMLwork=HTMLworkStart;
+			// create container for all single work details
+			var workDetails;
+			// fill work HTML with specific work data
+    		workDetails=HTMLworkEmployer.replace("%data%",JSONwork.jobs[i].employer)
+    		+HTMLworkTitle.replace("%data%",JSONwork.jobs[i].title)
+    		+HTMLworkDates.replace("%data%",JSONwork.jobs[i].dates)
+    		+HTMLworkLocation.replace("%data%",JSONwork.jobs[i].location)
+    		+HTMLworkDescription.replace("%data%",JSONwork.jobs[i].description);
+    		// add all details to the container
+    		HTMLwork=HTMLwork.insertAt(24,workDetails);
+    		// append job to the page
+    		$("#workExperience").append(HTMLwork);
+		}
+	}
+	else{console.log("work data not valid");}
+}
+
+function HideWork()
+{
+	if(document.getElementsByClassName('work-entry').length === 0) {
+      document.getElementById('workExperience').style.display = 'none';
+      console.log("work hidden");
+    }
+}
+
+$.ajax({
+url: "data/work.json",
+success: function (data) {
+  console.log("successfully loaded work JSON file");
+  FillWork(JSON.parse(data));
+  HideWork();
+},
+error: function (data) {
+  console.log("could not load work JSON file");
+  HideWork();
+}
+});
+
+// PROJECTS
+function FillProjects(JSONprojects)
+{
+	console.log("replacing project data");
+	if(JSONprojects.projects){
+		for (i = 0; i < JSONprojects.projects.length; i++) {
+			// create single project HTML
+			var HTMLproject=HTMLprojectStart;
+			// create container for all single project details
+			var projectDetails;
+			// fill work HTML with specific project data
+    		projectDetails=HTMLprojectTitle.replace("%data%",JSONprojects.projects[i].title)
+    		+HTMLprojectDates.replace("%data%",JSONprojects.projects[i].dates)
+    		+HTMLprojectDescription.replace("%data%",JSONprojects.projects[i].description);
+    		if(JSONprojects.projects[i].images)
+    		{
+    			for (j = 0; j < JSONprojects.projects[i].images.length; j++) {
+    				projectDetails=projectDetails+HTMLprojectImage.replace("%data%",JSONprojects.projects[i].images[j]);
+    			}
+    		}
+    		// add all details to the container
+    		HTMLproject=HTMLproject.insertAt(27,projectDetails);
+    		// append project to the page
+    		$("#projects").append(HTMLproject);
+		}
+	}
+	else{console.log("project data not valid");}
+}
+
+function HideProjects()
+{
+	if(document.getElementsByClassName('project-entry').length === 0) {
+      document.getElementById('projects').style.display = 'none';
+      console.log("projects hidden");
+    }
+}
+
+$.ajax({
+url: "data/projects.json",
+success: function (data) {
+  console.log("successfully loaded projects JSON file");
+  FillProjects(JSON.parse(data));
+  HideProjects();
+},
+error: function (data) {
+  console.log("could not load projects JSON file");
+  HideProjects();
+}
+});
